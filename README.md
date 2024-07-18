f75155fdef6371cc298fa59754d007b6079d1680
Matthew Peters <mpeters0514@gmail.com>
2024-07-18 01:01:56 -0400

    Initial Code Commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # QNNs
-Experiment with (pseudo/simulated) Quantum Neural Networks QNNs to learn both linear and nonlinear data
+
+This repo houses an expirement idea I had to learn both linear and nonlinear data using Quantum Neural Networks (QNNs).
+
+The idea came from [this](https://www.mathworks.com/help/matlab/math/solve-xor-problem-using-quantum-neural-network.html) reference when I noticed that the QNN implemented was able to learn nonlinear data (XOR data) without the need for a nonlinearity like Relu or tanh, etc... This got me wondering, could a QNN be trained to learn linear data as well ustilizing the same model structure?
+
+## Code
+
+Todo
+
+## Notes
+
+TBD
---
+++ src/nonlin.m
@@ -0,0 +1,59 @@
+function [X,Y] = generateData(numSamples)
+X = zeros(numSamples,2);
+labels = strings(numSamples,1);
+for i = 1:numSamples
+    x1 = 2*rand;
+    x2 = rand;
+    X(i,:) = [x1 x2];
+    if (x1 > 1 && x2 > 0.5) || (x1 < 1 && x2 < 0.5)
+        labels(i) = "Blue";
+    else
+        labels(i) = "Yellow";
+    end
+end
+Y = categorical(labels);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+numSamples = 200;
+[X,Y] = generateData(numSamples);
+classNames = ["Blue", "Yellow"];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+inputSize = size(X,2);
+numClasses = numel(classNames);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+addpath ../references
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+layers = [featureInputLayer(inputSize,Normalization="none")
+    PQCLayer
+    fullyConnectedLayer(numClasses)
+    softmaxLayer
+    classificationLayer];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+options = trainingOptions("sgdm", ...
+    MiniBatchSize=20, ...
+    InitialLearnRate=0.1, ...
+    Momentum=0.9, ...
+    ExecutionEnvironment="cpu", ...
+    Plots="training-progress", ...
+    Verbose=false);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+net = trainNetwork(X,Y,layers,options) %#ok<NOPTS>
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[XTest,trueLabels] = generateData(numSamples);
+predictedLabels = classify(net,XTest);
+gscatter(XTest(:,1),XTest(:,2),predictedLabels,"by")
+confusionchart(trueLabels,predictedLabels)
